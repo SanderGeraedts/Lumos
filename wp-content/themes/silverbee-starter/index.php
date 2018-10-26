d2c545a5da9d02df0cc728354794acfbee0b2b20
Sander Geraedts <sander@silverbee.nl>
2018-10-26 15:34:52 +0200

    added styles for archives

--- wp-content/themes/silverbee-starter/index.php
+++ wp-content/themes/silverbee-starter/index.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * The template for displaying archive pages
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -14,43 +9,52 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-
-			<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <div class="archive">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+							<?php
+							if ( have_posts() ) : ?>
+                                <div class="container">
+									<?php
+									/* Start the Loop */
+									while ( have_posts() ) : the_post(); ?>
+                                        <div class="row post-in-archive">
+                                            <div class="col-md-5"><?php the_post_thumbnail() ?></div>
+                                            <div class="col-md-7">
+                                                <div class="entry-meta">
+		                                            <?php silverbee_starter_posted_on(); ?>
+                                                </div><!-- .entry-meta -->
+                                                <h2>
+                                                    <a href="<?php the_permalink(); ?>" style="font-size: 16pt;">
+														<?php the_title(); ?>
+                                                    </a>
+                                                </h2>
+                                                <div class="entry-description" style="font-weight: 600;">
+													<?php the_excerpt(); ?>
+                                                </div>
+                                            </div>
+                                        </div>
+									<?php endwhile; ?>
+                                </div>
+								<?php the_posts_navigation(array(
+									"next_text" => "<i class=\"fas fa-chevron-left\"></i> Nieuwere berichten",
+									"prev_text" => "Oudere berichten <i class=\"fas fa-chevron-right\"></i>"
+								)); ?>
+							<?php else :
+								get_template_part( 'template-parts/content', 'none' );
+							endif; ?>
+                        </div>
+                        <div class="col-md-4">
+							<?php get_template_part( 'template-parts/sidebars/sidebar', 'page' ); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
